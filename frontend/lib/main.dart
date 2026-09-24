0afc5671c3ae89f318a30205361e5f76b7f66b03
sathurgini01 <188062846+sathurgini01@users.noreply.github.com>
2026-09-24 11:51:32 +0530

    YTH-29 add local saved posts

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'pages/share_story_page.dart';
 import 'pages/youth_feed_page.dart';
 import 'pages/youth_profile_page.dart';
+import 'pages/youth_saved_posts_page.dart';
 
 void main() {
   runApp(const YathraApp());
@@ -33,6 +34,7 @@ class YathraApp extends StatelessWidget {
       routes: {
         '/youth-feed': (context) => const YouthFeedPage(),
         '/youth-profile': (context) => const YouthProfilePage(),
+        '/youth-saved': (context) => const YouthSavedPostsPage(),
       },
       home: const ShareStoryPage(),
     );
